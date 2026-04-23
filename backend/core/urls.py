0aa6f5d25ac288b6c6eafa7bc1eba7399ffc88fb
dallas8000-ops <dallas8000-ops@github.com>
2026-04-22 23:09:32 -0400

    Serve media files in production for product image URLs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
@@ -22,6 +23,7 @@ urlpatterns = [
     path('cart/remove/<int:item_id>/', views.remove_cart_item, name='remove_cart_item'),
     path('admin/', admin.site.urls),
     path('api/inventory/', include('inventory.urls')),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
